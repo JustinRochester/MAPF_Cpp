af6ddfed8f787c612f1f0ae63ee39e48ad0cbb99
JustinRochester <923489297@qq.com>
2023-03-06 14:20:02 +0800

    adding the class of heuristic value map.

---
+++ inc/HeuristicMap.h
@@ -0,0 +1,64 @@
+//
+// Created by Administrator on 2023/3/4.
+//
+
+#ifndef MAPF_CPP_HEURISTICMAP_H
+#define MAPF_CPP_HEURISTICMAP_H
+
+#include <vector>
+#include "common.h"
+#include "Position.h"
+#include "Map.h"
+
+/**
+ *
+ * An grid-cell map with a heuristic value in each grid cells.
+ */
+class HeuristicMap {
+protected:
+    /**
+     *
+     * h_val[i][j] means the heuristic value of the cell which is located in (i,j).
+     */
+    std::vector< std::vector<HEURISTIC_TYPE> > h_val;
+
+public:
+    /**
+     *
+     * Initialize the heuristic map with a empty size(height = width = 0).
+     */
+    HeuristicMap();
+
+    /**
+     *
+     * @param maps detail information about this map.
+     * @param goal position of this single goal position.
+     * @param allowed_movements the allowed movements and its cost in this task.
+     *
+     * Calculate the heuristic in all the passable grid with the map_value and the single goal position.
+     * It should be ensured that goal position is passable.
+     */
+    void calculate_heuristic(Map &maps, Position goal,
+                             const std::vector<std::pair<Vector, HEURISTIC_TYPE>> &allowed_operations);
+
+    /**
+     *
+     * @param row which row of this heuristic map is selected.
+     * @return the reference of the selected row.
+     *
+     * It will throw an exception if the selected row is out of the bound.
+     */
+    std::vector<HEURISTIC_TYPE>& operator [] (int row);
+
+    /**
+     *
+     * @param row which row of this heuristic map is selected.
+     * @return the read-only reference of the selected row.
+     *
+     * It will throw an exception if the selected row is out of the bound.
+     */
+    const std::vector<HEURISTIC_TYPE>& operator [] (int row) const;
+};
+
+
+#endif //MAPF_CPP_HEURISTICMAP_H
---
+++ src/HeuristicMap.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Administrator on 2023/3/4.
+//
+
+#include "HeuristicMap.h"
+#include <queue>
+
+HeuristicMap::HeuristicMap() {
+    h_val.clear();
+}
+
+void HeuristicMap::calculate_heuristic(Map &maps, Position goal,
+                                       const std::vector<std::pair<Vector, HEURISTIC_TYPE>> &allowed_operations) {
+    int height = maps.get_height(), width = maps.get_width();
+    std::vector< std::vector<bool> > vised(height, std::vector<bool>(width));
+    h_val.assign(height, std::vector<HEURISTIC_TYPE>(width));
+
+    std::queue<Position> q;
+    q.push(goal);
+    vised[goal.gety()][goal.getx()] = true;
+    h_val[goal.gety()][goal.getx()] = 0;
+
+    while(!q.empty()) {
+        Position now_position = q.front();
+        q.pop();
+        HEURISTIC_TYPE now_h = h_val[now_position.gety()][now_position.getx()];
+        for(auto [move, cost] : allowed_operations) {
+            Position new_position = now_position + move;
+            if(new_position.gety() < 0 || new_position.gety() > height)
+                continue;
+            if(new_position.getx() < 0 || new_position.getx() > width)
+                continue;
+            if(vised[new_position.gety()][new_position.getx()])
+                continue;
+            vised[new_position.gety()][new_position.getx()] = true;
+            h_val[new_position.gety()][new_position.getx()] = now_h + cost;
+        }
+    }
+}
+
+std::vector<HEURISTIC_TYPE> &HeuristicMap::operator[](int row) {
+    if(row < h_val.size())
+        return h_val[row];
+    else
+        throw "Out of graph";
+}
+
+const std::vector<HEURISTIC_TYPE> &HeuristicMap::operator[](int row) const {
+    if(row < h_val.size())
+        return h_val[row];
+    else
+        throw "Out of graph";
+}
